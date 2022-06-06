31ccbb5fdb2fb36421ce64db25c2102be722a4a5
iibrahimli <imranibrahimli98@gmail.com>
2022-06-06 18:29:51 +0200

    add coliision reward and occupancy grid

--- gpairls/webots/robot_env/config.py
+++ gpairls/webots/robot_env/config.py
@@ -1,6 +1,8 @@
 """
 Webots robot environment config
 """
+from pathlib import Path
+
 
 # milliseconds
 CONTROL_TIMESTEP = 100
@@ -12,7 +14,16 @@ MAX_STEPS = round(10 * (1000 / CONTROL_TIMESTEP))
 STEP_REWARD = -1.
 
 # reward for reaching the goal
-GOAL_REWARD = 100.0
+GOAL_REWARD = 1000.0
+
+# reward for collision
+COLLISION_REWARD = -100.0
 
 # distance (meters) under which the goal is considered reached
 GOAL_DISTANCE_THRESHOLD = 0.1
+
+# occupancy grid resolution (meters)
+OCCUPANCY_GRID_RESOLUTION = 0.02
+
+# path to the occupancy grid
+OCCUPANCY_GRID_PATH = Path(__file__).parent.resolve() / "occupancy_grid.npy"
