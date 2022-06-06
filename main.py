95e8fadefa3e5e7bf4605c9f0d13f67e4f378e0f
iibrahimli <imranibrahimli98@gmail.com>
2022-06-06 18:28:44 +0200

    add testing script

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from gpairls.webots import RobotEnv
 env = RobotEnv()
 print("Initialized environment.")
 
-for episode in range(50):
+for episode in range(1):
     obs = env.reset()
     done = False
     episode_reward = 0
 
     while not done:
-        action = np.random.uniform(-0.1, 0.1)
-        obs, reward, done = env.step(action)
+        # action = np.random.uniform(-0.1, 0.1)
+        action = env.get_expert_action()
+        obs, reward, done, info = env.step(action)
         episode_reward += reward
+        # env.render(show_occupancy_grid=True)
+
+    print(f"[{episode}] Episode reward: {episode_reward}")
