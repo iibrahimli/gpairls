58d832f3cface92d5aa59ac278ca44739ca4a386
iibrahimli <imranibrahimli98@gmail.com>
2022-06-08 01:52:29 +0200

    modify config for webots

--- gpairls/config.py
+++ gpairls/config.py
@@ -15,23 +15,23 @@ MODEL_DIR = LOG_DIR / "model"
 SEED = 42
 
 # number of steps to train each agent
-TRAINING_STEPS = 20_000
+TRAINING_STEPS = 2000
 
 # number of steps to collect data before starting training
-INIT_STEPS = 1000
+INIT_STEPS = 500
 
 # encoder type
 ENCODER_TYPE = "cnn"
 
 # dimensionality of feature vector from encoder
-ENCODER_FEATURE_DIM = 4
+ENCODER_FEATURE_DIM = 32
 
 # actor and critic hidden dimensions
 HIDDEN_DIM = 16
 
-REPLAY_BUFFER_CAPACITY = 10_000
+REPLAY_BUFFER_CAPACITY = 128
 
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 
 CRITIC_LR = 1e-3
 
@@ -40,7 +40,7 @@ ACTOR_LR = 1e-3
 ENCODER_LR = 1e-3
 
 # frequency of logging (steps)
-LOG_FREQ = 100
+LOG_FREQ = 250
 
-# frequency of evaluation (episodes)
-EVAL_FREQ = 1
+# frequency of evaluation (steps)
+EVAL_FREQ = 250
