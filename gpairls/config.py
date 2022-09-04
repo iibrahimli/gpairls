c3f3587ff095a99112f92879291103dc588a35ac
iibrahimli <imranibrahimli98@gmail.com>
2022-09-04 16:50:20 +0200

    smaller models

--- gpairls/config.py
+++ gpairls/config.py
@@ -28,22 +28,22 @@ INIT_STEPS = 1000
 ENCODER_TYPE = "cnn"
 
 # dimensionality of feature vector from encoder
-ENCODER_FEATURE_DIM = 32
+ENCODER_FEATURE_DIM = 16
 
 # number of conv layers in the CNN encoder
 ENCODER_NUM_LAYERS = 3
 
 # number of filters in each encoder conv layer
-ENCODER_NUM_FILTERS = 128
+ENCODER_NUM_FILTERS = 64
 
 # actor and critic hidden dimensions
-HIDDEN_DIM = 64
+HIDDEN_DIM = 32
 
 # neurons in reward decoder hidden layers
-DECODER_DIM = 64
+DECODER_DIM = 32
 
 # neurons in transition model hidden layer
-TRANSITION_MODEL_DIM = 64
+TRANSITION_MODEL_DIM = 32
 
 REPLAY_BUFFER_CAPACITY = 10_000
 
